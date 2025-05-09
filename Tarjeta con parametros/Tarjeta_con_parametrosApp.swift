88072841c95eaa89320b622f91ab6f82b0daf044
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-09 12:25:34 -0600

    Initial Commit

---
+++ Tarjeta con parametros/Tarjeta_con_parametrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tarjeta_con_parametrosApp.swift
+//  Tarjeta con parametros
+//
+//  Created by Benjamin Rojo on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Tarjeta_con_parametrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
