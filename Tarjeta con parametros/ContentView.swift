98a6268da4423466aa1737633421f6a143ea2804
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-12 11:24:29 -0600

    i change evrything and add another view inside of the VStack

--- Tarjeta con parametros/ContentView.swift
+++ Tarjeta con parametros/ContentView.swift
@@ -7,46 +7,47 @@
 
 import SwiftUI
 
+// Vista principal
 struct ContentView: View {
-    var nombreUsuario1 = "Frank Casillas"
-    var rol1 = "iOS Developer"
-    let fondo1 = Color.green
     var body: some View {
-        VStack {
-            Image(systemName: "person.circle.fill")
-                .font(.system(size: 40, weight: .bold))
-                .foregroundColor(.green)
-            Text("This is the developer  \(nombreUsuario1)")
-                .font(.headline)
-            Text("He is a  \(rol1)")
-ProfileCardView()
+        VStack(spacing: 20) {
+            //Las dos tarjetas
+            ProfileCardView(nombreUsuario: "Frank Casillas", rol: "iOS Developer", fondo: .green)
+
+            ProfileCardView(nombreUsuario: "Sofía García", rol: "UX Designer", fondo: .red)
         }
         .padding()
-        .background(fondo1)
-        .cornerRadius(15)
-     
     }
 }
-struct ProfileCardView:View{
-    var nombreUsuario = "Benjamin Salgado"
-    var rol = "iOS Developer"
-    let fondo = Color.red
-    var body: some View{
-        VStack{
+
+// Vista personalizada para cada tarjeta
+struct ProfileCardView: View {
+    // variables y condicionales para los valores de la tarjeta
+    var nombreUsuario: String
+    var rol: String
+    let fondo: Color
+
+    var body: some View {
+        VStack(spacing: 10) {
             Image(systemName: "person.circle.fill")
-                .font(.system(size: 40, weight: .bold))
-                .foregroundColor(.blue)
-            Text("This is the developer  \(nombreUsuario)")
-                .font(.headline)
-            Text("He is a  \(rol)")
+                .font(.system(size: 80))
+                .foregroundColor(.white)
+
+            Text(nombreUsuario)
+                .font(.title2)
+                .bold()
+                .foregroundColor(.white)
+
+            Text(rol)
                 .font(.subheadline)
-         
+                .foregroundColor(.white)
         }
         .padding()
-                .background(fondo)
-                .cornerRadius(15)
+        .background(fondo)
+        .cornerRadius(50)
     }
 }
+
 #Preview {
     ContentView()
 }
